6071cd0f73368c004491714154a7501cf2ab5a54
Sc4rS <36574892+Sc4rS@users.noreply.github.com>
2018-10-22 02:21:34 +0200

    Update src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireStorageModule } from 'angularfire2/storage';
 import { environment } from '../environments/environment';
 import { ItemsComponent } from './components/items/items.component';
 import { ItemService } from './services/item.service';
@@ -23,10 +22,10 @@ import { AddItemComponent } from './components/add-item/add-item.component';
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase, 'products'),
     AngularFirestoreModule,
-    AngularFireStorageModule,
   ],
   providers: [
-      ItemService
+      ItemService,
+
   ],
   bootstrap: [AppComponent]
 })
